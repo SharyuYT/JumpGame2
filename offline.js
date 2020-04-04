9e142a05a2db0829c2332c574d5116e900d53166
Adrian <hkhh@Adrian.Adrian>
2020-04-04 23:37:47 +0200

    Initial commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1586036151,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tile_grass-sheet0.png",
+		"images/female_stand-sheet0.png",
+		"images/background-sheet0.png",
+		"images/gold_-sheet0.png",
+		"images/gold_-sheet1.png",
+		"images/gold_-sheet2.png",
+		"media/hop.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
